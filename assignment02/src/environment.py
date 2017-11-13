cb8af5e2f87f8ddcf5a1a2d9a2924e5074ac366d
marisanest <marisanest@mailbox.org>
2017-11-13 19:00:15 +0100

    Reorganised project structure

---
+++ assignment02/src/environment.py
@@ -0,0 +1,11 @@
+class JacksCarRentalEnvironment(object):
+
+    def __init__(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def step(self):
+        observation, reward, done, info = None
+        return observation, reward, done, info
