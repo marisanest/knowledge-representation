e31d55835dedfdb20ab64e04e2eb8b366406a452
marisanest <marisanest@mailbox.org>
2017-11-25 17:15:00 +0100

    mdp

--- assignment02/src/jacks_car_rental/mdp.py
+++ assignment02/src/jacks_car_rental/mdp.py
@@ -3,11 +3,14 @@ from assignment02.src.jacks_car_rental.model import JacksCarRentalEnvironmentMod
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 # from matplotlib import cm
+import logging
 
 
 class JacksCarRentalEnvironmentMDP(object):
 
     def __init__(self):
+        logging.basicConfig(level=logging.INFO)
+
         self.model = JacksCarRentalEnvironmentModel()
 
         self.nb_separate_states = self.model.MAX_CAPACITY + 1
@@ -18,7 +21,8 @@ class JacksCarRentalEnvironmentMDP(object):
 
         self.policy = self._init_policy()
         self.q = self._init_q()
-        self.p, self.r = self._init_p_and_r()
+        # self.p, self.r = self._init_p_and_r()
+        self.v = None
 
     def _init_index_to_stats(self):
 
@@ -55,15 +59,27 @@ class JacksCarRentalEnvironmentMDP(object):
     def _init_q(self):
         return np.zeros((self.nb_states, self.nb_actions))
 
+    def _calculate_v(self):
+
+        self.v = np.zeros(self.nb_states)
+
+        for s in range(self.nb_states):
+            self.v[s] = np.amax(self.q[s, :])
+
     def evaluate(self, theta=.05, gamma=.9):
 
         converged = False
 
         while not converged:
 
+            logging.info("values not converged.")
+            logging.info("start evaluation.")
+
             delta = .0
 
             for s in range(0, self.nb_states):
+
+                logging.info("evaluation for state: %s" % s)
 
                 a = self.policy[s]
                 old_q = self.q[s, a]
@@ -75,10 +91,12 @@ class JacksCarRentalEnvironmentMDP(object):
                 self.q[s, a] = new_q
                 delta = np.amax([delta, abs(old_q - new_q)])
 
-            if delta >= theta:
+            if delta < theta:
                 converged = True
 
     def improve(self):
+
+        logging.info("start improvement.")
 
         for s in range(0, self.nb_states):
             max_a, max_value = None, None
@@ -93,16 +111,21 @@ class JacksCarRentalEnvironmentMDP(object):
 
             self.policy[s] = max_a
 
-    def iterate_policy(self):
+    def iterate_policy(self, theta=.05):
 
         policy_stable = False
 
+        logging.info("start iteration")
+
         while not policy_stable:
 
-            self.evaluate()
+            logging.info("policy not stable.")
 
-            old_policy = self.policy
+            self.evaluate(theta=theta)
 
+            old_policy = np.copy(self.policy)
+
+            logging.info("start improvement.")
             self.improve()
 
             if (old_policy == self.policy).all():
@@ -112,11 +135,18 @@ class JacksCarRentalEnvironmentMDP(object):
 
         converged = False
 
+        logging.info("start iteration")
+
         while not converged:
+
+            logging.info("values not converged.")
+            logging.info("start evaluation.")
 
             delta = .0
 
             for s in range(0, self.nb_states):
+
+                logging.info("evaluation for state: %s" % s)
 
                 old_max_q = np.amax(self.q[s, :])
                 old_a = np.argmax(self.q[s, :])
@@ -133,20 +163,23 @@ class JacksCarRentalEnvironmentMDP(object):
                 new_max_q = np.amax(self.q[s, :])
                 delta = np.amax([delta, abs(old_max_q - new_max_q)])
 
-            if delta >= theta:
+            if delta < theta:
                 converged = True
 
         self.improve()
 
     def plot3d_over_states(self, z_label="", ):
 
-        a = np.arange(0, self.nb_states)
-        b = np.arange(0, self.nb_states)
+        if self.v is None:
+            self._calculate_v()
+
+        a = np.arange(0, self.nb_separate_states)
+        b = np.arange(0, self.nb_separate_states)
 
         # b, a !!!
         b, a = np.meshgrid(b, a)
 
-        v = self.q.reshape(self.nb_states, -1)
+        v = self.v.reshape(self.nb_separate_states, -1)
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
 
@@ -166,12 +199,12 @@ class JacksCarRentalEnvironmentMDP(object):
 
     def plot_policy(self):
 
-        a = np.arange(0, self.nb_states)
-        b = np.arange(0, self.nb_states)
+        a = np.arange(0, self.nb_separate_states)
+        b = np.arange(0, self.nb_separate_states)
 
         a, b = np.meshgrid(a, b)
 
-        po = self.policy.reshape(self.nb_states, -1)
+        po = self.policy.reshape(self.nb_separate_states, -1)
         levels = range(-5, 6, 1)
         plt.figure(figsize=(7, 6))
 
