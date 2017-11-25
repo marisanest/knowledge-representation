c16a4406f30ac133ddd6b671a2ae3c1668f34717
marisanest <marisanest@mailbox.org>
2017-11-25 19:17:58 +0100

    main

--- assignment02/src/jacks_car_rental/mdp.py
+++ assignment02/src/jacks_car_rental/mdp.py
@@ -22,14 +22,16 @@ class JacksCarRentalEnvironmentMDP(object):
         self.policy = self._init_policy()
         self.q = self._init_q()
         self.p, self.r = self._init_p_and_r()
+
         self.v = None
+        self.state_to_action = None
 
     def _init_index_to_stats(self):
 
         index_to_states = {}
 
-        for index_a in range(0, self.nb_separate_states):
-            for index_b in range(0, self.nb_separate_states):
+        for index_a in range(self.nb_separate_states):
+            for index_b in range(self.nb_separate_states):
                 index_to_states[index_a * self.nb_separate_states + index_b] = (index_a, index_b)
 
         return index_to_states
@@ -43,8 +45,8 @@ class JacksCarRentalEnvironmentMDP(object):
         p = np.zeros((self.nb_states, self.nb_actions, self.nb_states, 1))
         r = np.zeros((self.nb_states, self.nb_actions))
 
-        for s in range(0, self.nb_states):
-            for a in range(0, self.nb_actions):
+        for s in range(self.nb_states):
+            for a in range(self.nb_actions):
 
                 p_a_b, r[s, a] = self.model.get_transition_probabilities_and_expected_reward(self.index_to_stats[s], self.model.ACTIONS[a])
                 for next_s_a, p_a in enumerate(p_a_b[0]):
@@ -59,23 +61,29 @@ class JacksCarRentalEnvironmentMDP(object):
     def _init_q(self):
         return np.zeros((self.nb_states, self.nb_actions))
 
-    def _calculate_v(self):
+    def _init_v(self):
 
         self.v = np.zeros(self.nb_states)
 
         for s in range(self.nb_states):
             self.v[s] = np.amax(self.q[s, :])
 
+    def _init_policy_to_action(self):
+
+        self.state_to_action = np.zeros(self.nb_states)
+
+        for key, value in enumerate(self.policy):
+            self.state_to_action[key] = self.model.ACTIONS[value]
+
     def evaluate(self, gamma=.9):
+
+        logging.info("start evaluation.")
 
         l = range(self.nb_states)
 
         converged = False
 
         while not converged:
-
-            logging.info("values not converged.")
-            logging.info("start evaluation.")
 
             v = self.q[l, self.policy]
             new_q = self.r + gamma * np.dot(self.p.sum(axis=3), v)
@@ -86,21 +94,20 @@ class JacksCarRentalEnvironmentMDP(object):
             self.q = new_q
 
     def improve(self):
-        self.policy = np.argmax(self.q[:, 1:], axis=1) + 1
+
+        logging.info("start improvement.")
+
+        self.policy = np.argmax(self.q[:, :], axis=1)
 
     def iterate_policy(self):
 
-        policy_stable = False
-
         logging.info("start iteration")
+
+        policy_stable = False
 
         while not policy_stable:
 
-            logging.info("policy not stable.")
-
             self.evaluate()
-
-            logging.info("start improvement.")
 
             old_policy = np.copy(self.policy)
 
@@ -111,12 +118,11 @@ class JacksCarRentalEnvironmentMDP(object):
 
     def iterate_values(self, gamma=.9):
 
+        logging.info("start iteration")
+
         converged = False
 
         while not converged:
-
-            logging.info("values not converged.")
-            logging.info("start evaluation.")
 
             v_max = np.max(self.q, axis=1)
             new_q = self.r + gamma * np.dot(self.p.sum(axis=3), v_max)
@@ -130,7 +136,7 @@ class JacksCarRentalEnvironmentMDP(object):
     def plot3d_over_states(self, z_label="", ):
 
         if self.v is None:
-            self._calculate_v()
+            self._init_v()
 
         a = np.arange(0, self.nb_separate_states)
         b = np.arange(0, self.nb_separate_states)
@@ -158,13 +164,16 @@ class JacksCarRentalEnvironmentMDP(object):
 
     def plot_policy(self):
 
+        if self.state_to_action is None:
+            self._init_policy_to_action()
+
         a = np.arange(0, self.nb_separate_states)
         b = np.arange(0, self.nb_separate_states)
 
         a, b = np.meshgrid(a, b)
 
-        po = self.policy.reshape(self.nb_separate_states, -1)
-        levels = range(-5, 6, 1)
+        po = self.state_to_action.reshape(self.nb_separate_states, -1)
+        levels = range(min(self.model.ACTIONS), max(self.model.ACTIONS) + 1 , 1)
         plt.figure(figsize=(7, 6))
 
         cs = plt.contourf(a, b, po, levels)
